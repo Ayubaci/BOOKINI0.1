53ddd9939af43c7fe6b042480cd8930a39a6dd05
ayubaci <ayoubaci2002@gmail.com>
2023-12-31 19:07:24 +0100

    create a simple bookshelf searching system

--- bookshelf.php
+++ bookshelf.php
@@ -28,6 +28,10 @@ include("header.html");
                     <input type="radio" name="major" value="computerscience" id="computerscience">
                     <label for="computerscience">Computer Science</label>
                 </div>
+                <div class="radio-input-container">
+                    <input type="radio" name="major" value="webprogramming" id="webprogramming">
+                    <label for="webprogramming">Web Programming</label>
+                </div>
                 <br>
 
                 <!-- creating our filters by modules-->
@@ -55,18 +59,42 @@ include("header.html");
             include("connection.php");
             $sql = "SELECT * FROM books";
             $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
+            // checking if the user applied anny fillters :
+            if (isset($_POST["apply"])) {
+                $filter = $_POST["major"];
+                $sql_filter = "select * from books where field = '$filter'";
+                $filter_result = $conn->query($sql_filter);
+                if ($filter_result->num_rows > 0) {
+                    while ($row = $filter_result->fetch_assoc()) {
+                        //checking if the book is avialable:
+                        if ($row["qnt"] >= 1) {
+                            // Access individual fields using associative array keys
+                            echo '<div class="a-book-card">
+                                    <img class="book-img" src=' . $row["imgpath"] . '>
+                                    <h3>Title: ' . $row["title"] . '</h3>
+                                    <h4>Price: ' . $row["price"] . ' DA</h4>
+                                    <form action="bookshelf.php" method="post">
+                                        <input class="order-btn" type="submit" value="Order" name="order">
+                                    </form>
+                                </div>';
+                        }
+                    }
+                }
+            } elseif ($result->num_rows > 0) {
                 // Loop through each row in the result set
                 while ($row = $result->fetch_assoc()) {
-                    // Access individual fields using associative array keys
-                    echo '<div class="a-book-card">
-                            <img src=' . $row["imgpath"] . '>
-                            <h3>Title: ' . $row["title"] . '</h3>
-                            <form action="bookshelf.php" method="post">
-                                <input class="order-btn" type="submit" value="Order" name="order">
-                            </form>
-                        </div>';
+                    //checking if the book is avialable:
+                    if ($row["qnt"] >= 1) {
+                        // Access individual fields using associative array keys
+                        echo '<div class="a-book-card">
+                                <img class="book-img" src=' . $row["imgpath"] . '>
+                                <h3>Title: ' . $row["title"] . '</h3>
+                                <h4>Price: ' . $row["price"] . ' DA</h4>
+                                <form action="bookshelf.php" method="post">
+                                    <input class="order-btn" type="submit" value="Order" name="order">
+                                </form>
+                            </div>';
+                    }
                 }
             } else {
                 echo "No results";
